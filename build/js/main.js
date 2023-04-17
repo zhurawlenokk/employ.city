1cba8c4b7b6c5f8a9e014bf9470ac7f0c5f49b6b
zhuravlenokk <zhuravleva@jam-it.ru>
2023-04-17 10:41:40 +0400

    build folder

---
+++ build/js/main.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //открытие списка в селекте в форме
+    function showSelectionsList() {
+        var input = document.querySelector('.order-form-selection'),
+            list = document.querySelector('.select-list');
+
+        if (input) {
+            var closeSelectByEmptySpace = function closeSelectByEmptySpace() {
+                // закрытие по клику на пустом месте
+                document.addEventListener('click', function(e) {
+                    if (list.classList.contains('select-list--active')) {
+                        var withinSelectWrap = e.composedPath().includes(list);
+                        var withinSelectInput = e.composedPath().includes(input);
+
+                        if (!withinSelectWrap && !withinSelectInput) {
+                            toggleFilterSelectList();
+                            input.closest('.order-form-select').classList.remove('rotate-arrow');
+                        }
+                    }
+                });
+            };
+
+            var toggleFilterSelectList = function toggleFilterSelectList() {
+                list.classList.toggle('select-list--active');
+            };
+
+            input.addEventListener('click', function() {
+                list.classList.toggle('select-list--active');
+                input.closest('.order-form-select').classList.toggle('rotate-arrow');
+            });;;
+            closeSelectByEmptySpace();
+        }
+
+        ;
+    }
+
+    showSelectionsList(); //подсчет процентов
+
+    var getValueInpR = function getValueInpR() {
+        var perValue = document.getElementById('percent'),
+            printPerValue = document.querySelector('.percent-input-value');
+        printPerValue.innerText = "".concat(perValue.value, "%");
+    };
+
+    percent.addEventListener('input', getValueInpR);
+
+    window.onunload = function() {
+        return percent.removeEventListener('input', getValueInpR);
+    }; //фиксация меню при скролле
+
+
+    var fixedBar = document.querySelector('.header-menu');
+
+    var toggleBar = function toggleBar() {
+        var isShown = window.pageYOffset > window.innerHeight / 2;
+        fixedBar.classList.toggle('isShown', isShown);
+    };
+
+    toggleBar();
+    window.addEventListener('scroll', toggleBar); 
+    
+    //открытие меню в мобилке
+    function openMenu() {
+        var menuButton = document.querySelector('.burger-menu'),
+            menu = document.querySelector('.header-menu-nav');
+        menuButton.addEventListener('click', function() {
+            menu.classList.toggle('header-menu-nav--active');
+        });
+    }
+
+    openMenu();
+});
